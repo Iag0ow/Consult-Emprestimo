c8164bc4bda22bf90defd9e0bb3ef6948d20671d
Tony <tony.albert789@gmail.com>
2023-07-09 22:35:10 -0300

    Ajuste form

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -179,27 +179,27 @@
 {{-- Mobile --}}
 <section id="formulario-simulacao-mobile" class="flex w-screen h-screen bg-cover bg-center md:hidden bg-[#DFEEF6] px-[40px] md:px-64" style="background-image: url('./assets/images/background3.png');">
     <div class="my-auto">
-        <div class="bg-white px-6 py-5 pb-14 rounded-xl drop-shadow-md">
+        <div class="bg-white px-6 py-4 pb-14 rounded-xl drop-shadow-md">
           <h2 class="text-center text-md font-extrabold">Empréstimo <span class="font-normal">de até</span> R$ 50.000</h2>
           <p class="text-center text-md font-extrabold text-[#23A6F0] mb-5">Simule já</p>
   
           <form class="ajaxForm2" method="POST">
             @csrf
             <div class="flex flex-wrap">
-              <input type="text" name="nome" placeholder="Nome" autocomplete="off" class="mb-3 w-full px-3 py-3 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-2 focus:outline-none transition duration-500">
-              <input type="text" name="cnpj" placeholder="CNPJ" autocomplete="off" class="cnpj mb-3 w-full px-3 py-3 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-2 focus:outline-none transition duration-500">
-              <input type="text" name="faturamento" placeholder="Faturamento" autocomplete="off" class="faturamento mb-3 w-full px-3 py-3 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-2 focus:outline-none transition duration-500">
+              <input type="text" name="nome" placeholder="Nome" autocomplete="off" class="text-xs mb-2 w-full px-2 py-2 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-1 focus:outline-none transition duration-500">
+              <input type="text" name="cnpj" placeholder="CNPJ" autocomplete="off" class="cnpj text-xs mb-2 w-full px-2 py-2 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-1 focus:outline-none transition duration-500">
+              <input type="text" name="faturamento" placeholder="Faturamento" autocomplete="off" class="faturamento text-xs  mb-2 w-full px-2 py-2 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-1 focus:outline-none transition duration-500">
             </div>
   
             <div class="grid grid-cols-6 gap-3">
-              <input type="tel" name="ddd" placeholder="DDD" class="ddd2 col-span-2 mb-2 w-full px-3 py-3 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-2 focus:outline-none transition duration-500">
-              <input type="tel" name="numero" placeholder="Telefone" class="js-phoneMask2 col-span-4 mb-2 w-full px-3 py-3 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-2 focus:outline-none transition duration-500">
+              <input type="tel" name="ddd" placeholder="DDD" class="ddd2 text-xs col-span-2 mb-2 w-full px-2 py-2 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-1 focus:outline-none transition duration-500">
+              <input type="tel" name="numero" placeholder="Telefone" class="js-phoneMask2 text-xs col-span-4 mb-2 w-full px-2 py-2 border border-[#D9D9D9] rounded-lg focus:drop-shadow-lg focus:border-[#23A6F0] focus:border-1 focus:outline-none transition duration-500">
             </div>
   
             <div class="flex mt-2">
               <input class="mr-2" type="checkbox" name="whatsapp" id="whatsapp">
-              <label class="text-[#B6B6B6] mr-2" for="whatsapp">WhatsApp</label>
-              <img src="./assets/images/whatsapp.svg" alt="">
+              <label class="text-[#B6B6B6] my-auto text-xs mr-2" for="whatsapp">WhatsApp</label>
+              <img style="width: 16px;" src="./assets/images/whatsapp.svg" alt="">
             </div>
   
           </form>
