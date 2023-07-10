058233875a17aaeea21725ea34d8d822a990ecf8
Tony <tony.albert789@gmail.com>
2023-07-09 21:09:32 -0300

    Ajuste responsivo

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -16,6 +16,9 @@
       tailwind.config = {
         theme: {
             extend: {
+            screens: {
+                'md': '1000px', // Altere o valor para o tamanho desejado
+            },
             colors: {
                 clifford: '#da373d',
             },
